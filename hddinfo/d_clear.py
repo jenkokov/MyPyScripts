b042aa7ba89edbba6588bf1eb96fe065da1ee288
Yevgeniy Kovalevskiy <jenko.kov@gmail.com>
2012-08-31 11:00:17 +0300

    Revision#9

--- hddinfo/d_clear.py
+++ hddinfo/d_clear.py
@@ -9,9 +9,15 @@ def main():
     for name in all_folders:
         if name not in need_folders:
             if os.path.isdir(work_folder+ name) == 1:
-                shutil.rmtree(work_folder+ name, 1)
+                try:
+                    shutil.rmtree(work_folder+ name, 1)
+                except:
+                    print 'Error deleting {0}'.format(work_folder+name)
             else:
-                os.remove(work_folder+ name)
+                try:
+                    os.remove(work_folder+ name)
+                except:
+                    print 'Error deleting {0}'.format(work_folder+name)
 if __name__ == '__main__':
     if len(sys.argv)<2:
         print 'Need are some one any parameter for cleaning!'
