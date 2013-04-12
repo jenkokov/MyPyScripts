8b1b40deb9e208e682dfecbbf421f499e90673a3
Yevgen Kovalevskiy <jenko.kov@gmail.com>
2013-04-12 17:43:37 +0300

    Revision#58

--- itl_auto/avp.py
+++ itl_auto/avp.py
@@ -1,21 +1,33 @@
 # -*- coding: utf-8 -*-
 from subprocess import call
-from subprocess import check_call as w_call
-from subprocess import check_output as n_call
 import os
 import shutil
 import glob
+import socket
+
+ip = socket.gethostbyname(socket.gethostname())
+club = ip.split('.')[2]
+comp = ip.split('.')[3]
+
+
+def run(path):
+    try:
+        call(path, shell=True)
+    except:
+        os.system(path)
 
 
 def delete(name):
     if os.path.exists(name):
         if os.path.isdir(name) == 1:
             try:
+                print 'Deleting ' + name
                 shutil.rmtree(name, True)
             except:
                 print 'Error deleting {0}'.format(name)
         else:
             try:
+                print 'Deleting ' + name
                 os.remove(name)
             except:
                 print 'Error deleting {0}'.format(name)
@@ -24,7 +36,7 @@ def delete(name):
 
 
 def clean():
-    f = open('clean_block.ini')
+    f = open(r'\\172.16.10.252\icons\clean_block.ini')
     for path in f:
         name = path[:-1]
         if name[-1] == '*':
