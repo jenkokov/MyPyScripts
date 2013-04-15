d0131d689cef1a5aa32eb803752dce5f215483f4
Yevgen Kovalevskiy <jenko.kov@gmail.com>
2013-04-15 12:28:48 +0300

    Revision#59

--- itl_auto/avp.py
+++ itl_auto/avp.py
@@ -4,10 +4,14 @@ import os
 import shutil
 import glob
 import socket
+import ConfigParser
+from multiprocessing import Process
 
+config = ConfigParser.RawConfigParser()
 ip = socket.gethostbyname(socket.gethostname())
 club = ip.split('.')[2]
 comp = ip.split('.')[3]
+server = '.'.join(ip.split('.')[:-1]) + '.252'
 
 
 def run(path):
@@ -15,6 +19,13 @@ def run(path):
         call(path, shell=True)
     except:
         os.system(path)
+
+
+def n_run(path):
+    p = Process(target=run, args=(path,))
+    p.start()
+    p.join()
+    return
 
 
 def delete(name):
@@ -36,9 +47,11 @@ def delete(name):
 
 
 def clean():
-    f = open(r'\\172.16.10.252\icons\clean_block.ini')
+    f = open(r'\\{0}\icons\clean_block.ini'.format(server))
     for path in f:
         name = path[:-1]
+        if name[0] == '#':
+            continue
         if name[-1] == '*':
             files = glob.glob(name)
             for f in files:
@@ -47,9 +60,14 @@ def clean():
             delete(name)
 
 
-def main():
-    clean()
+def run_module():
+    n_run(r'\\{0}\standartgames\Settings\DeadSpace2_pub_files.exe'.format(server))
+    run(r'\\{0}\standartgames\Settings\CrimeCraft_setting.exe'.format(server))
 
+
+def main():
+    #clean()
+    run_module()
 
 if __name__ == '__main__':
     main()
